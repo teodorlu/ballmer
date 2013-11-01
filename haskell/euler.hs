3dd2ea18cfac0cde8d3e176c7566dde5c889031a
Teodor Heggelund <teodor.heggelund@gmail.com>
2013-11-01 03:14:39 +0100

    Done on one

--- haskell/euler.hs
+++ haskell/euler.hs
@@ -2,11 +2,19 @@ import Data.Char
 
 main = do
 	content <- readFile("words.txt")
-	putStrLn $ show $ map numbers $ words $ nice content
+	putStrLn $ show $ length $ filter isTriangleNum $ map sum $ map numbers $ words $ nice content
 
 numbers = map (\c -> (ord c) - (ord 'A') + 1)
 nice c = filter (/='"') $ map repl c
 
-
 repl ',' = ' '
 repl  s  =  s
+
+triangle n = n * (n-1) `quot` 2
+
+isTriangleNumR n i
+	| triangle i  > n = False
+	| triangle i == n = True
+	| otherwise		  = isTriangleNumR n (i+1)
+
+isTriangleNum n = isTriangleNumR n 1
