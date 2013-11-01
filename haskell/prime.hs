d696f8d7fb7134a1252c48af23af3a8450f05a1f
Håkon Bråten <haakobra@stud.ntnu.no>
2013-11-01 01:36:35 +0100

    prime.hs fungerer nå

--- haskell/prime.hs
+++ haskell/prime.hs
@@ -1,3 +1,2 @@
-prime n = not $ any (==0) $ map (mod n) [2.. n-1]
-main = putStrLn $ show $ map prime [1.. 20]
-
+prime n = not $ any (==0) $ map (mod (round n)) [2.. round (sqrt $ (n-1))]
+main = putStrLn $ show $ zip [1..] $ map prime [1.. 20]
