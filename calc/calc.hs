4bb92f4b306f36327cbf2b29e5f03bd3691f8375
teodorlu <teodorlu@users.noreply.github.com>
2013-11-12 15:31:34 +0100

    Update calc.hs

--- calc/calc.hs
+++ calc/calc.hs
@@ -1,8 +1,6 @@
 evaluate :: Num a => [String] -> [a]
 evaluate ("add":h1:h2:t) = ((read h1) + (read h2)):evaluate t
 
-evaluate xs = [5, 5]
-
 worker = do
 	input <- getLine
 	putStrLn $ show . evaluate . words $ input
